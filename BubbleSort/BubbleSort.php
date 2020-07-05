9334ed314b2018afe10effdd57cbc0c411f7440e
cuong6218 <cuong6218@domain.com>
2020-07-05 15:18:49 +0700

    add file bubble sort

---
+++ BubbleSort/BubbleSort.php
@@ -0,0 +1,20 @@
+<?php
+
+function bubbleSort($arr)
+{
+    $count = count($arr);
+    for ($i = 0; $i < $count; $i++) {
+        for ($j = 0; $j < $count - $i - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j + 1];
+                $arr[$j + 1] = $arr[$j];
+                $arr[$j] = $temp;
+            }
+        }
+    }
+    for ($i = 0; $i < $count; $i++) {
+        echo $arr[$i] . " ";
+    }
+}
+$arr = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+bubbleSort($arr);
